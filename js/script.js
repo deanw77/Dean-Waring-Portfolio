3f103b8ddede44c08345273bc7cb54cee25a6bd3
deanw77 <deanwaring77@outlook.com>
2023-10-29 14:27:11 +0000

    Basic Layout Complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const offScreenBtns = document.querySelector('.offScreenBtns');
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
+    offScreenBtns.classList.toggle('active');
 });
