8d09bd3cecc92300beec948e29092834eafab111
deanw77 <deanwaring77@outlook.com>
2023-10-30 07:22:13 +0000

    Created too many errors overcomplicating things, back to basics

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const hamMenu = document.querySelector('.hamburger-menu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-const offScreenBtns = document.querySelector('.offScreenBtns');
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-    offScreenBtns.classList.toggle('active');
-});
